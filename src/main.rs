df89160136938fe99cc86a840aa91e0854f5d676
raphaelklein <raphael.klein@tum.de>
2024-03-21 05:17:31 +0100

    comment and readme update

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,42 @@
-//compiler attribute
+// compiler attribute
 #![no_std]
-//no main because we don't want to use the normal entry point chain (Rust runtime, start from crt0...)
+// no main because we don't want to use the normal entry point chain (Rust runtime, start from crt0...)
 #![no_main]
 
-//basically import of PanicInfo
+// import of PanicInfo
 use core::panic::PanicInfo;
 
-//#[] are attributes that are used to mark stuff for the current crate
+// #[] are attributes that are used to mark stuff for the current crate
 #[panic_handler]
-//_ is convention because info parameter is not used
-//-> ! specifies return type of function. ! is the "never type" indicating returning NOTHING, not even control to the caller
-//(different to "void" equivalent of omitting the return type
+// _ is convention because info parameter is not used
+// -> ! specifies return type of function. ! is the "never type" indicating returning NOTHING, not even control to the caller
+// (different to "void" equivalent of omitting the return type
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
 static HELLO: &[u8] = b"Hello World!";
 
-//overwrite of entry point
-//no mangle to stop Rust from name mangling and outputs _start
+// overwrite of entry point
+// no mangle to stop Rust from name mangling and outputs _start
 #[no_mangle]
-//"C" to use C calling convention
+// "C" to use C calling convention
 pub extern "C" fn _start() -> ! {
-    //casting the int of our mem address of the VGA buffer into a raw pointer
+    // casting the int of our mem address of the VGA buffer into a raw pointer
     let vga_buffer = 0xb8000 as *mut u8;
 
-    //iterating over bytes of HELLO string
+    // iterating over bytes of HELLO string
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
-            //writing string byte and color byte
+            // writing string byte and color byte
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
     loop {}
 }
-//--we need to build for a target triple with no underlying OS for now--
-//--to build use: cargo build --target thumbv7em-none-eabihf--
-//--(install target triple: rustup target add thumbv7em-none-eabihf)--
+// --we need to build for a target triple with no underlying OS for now--
+// --to build use: cargo build --target thumbv7em-none-eabihf--
+// --(install target triple: rustup target add thumbv7em-none-eabihf)--
 
-//we need nighly mode: rustup override set nightly
+// we need nighly mode: rustup override set nightly
